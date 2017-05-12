d375a282cfadac0f58bf9675e80fb8f7590a038f
Gianmarco <giorgian.g@gmail.com>
2017-05-12 06:26:16 -0500

    reduxstagram terminado

--- Project/client/components/Main.js
+++ Project/client/components/Main.js
@@ -1,15 +1,21 @@
-import React from "react"
-import { Link } from "react-router"
+import React from 'react';
+import { Link } from 'react-router';
 
 const Main = React.createClass({
-  render(){
-    return(
+
+  render() {
+    // Then we go ahead and return some JSX
+    return (
       <div>
         <h1>
           <Link to="/">Reduxstagram</Link>
         </h1>
+        {/* We use cloneElement here so we can auto pass down props */}
+        { React.cloneElement(this.props.children, this.props) }
       </div>
-    )
+    );
   }
-})
-export default Main
+
+});
+
+export default Main;
